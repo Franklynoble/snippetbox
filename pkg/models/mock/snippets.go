559f6745cf4473cece26c1a500a72401cb7314a5
Franklynoble <Franklynoblez@gmail.com>
2022-04-14 13:29:35 -0400

    add handler showsnippet test

---
+++ pkg/models/mock/snippets.go
@@ -0,0 +1,30 @@
+package mock
+
+import (
+	"time"
+
+	"github.com/Franklynoble/snippetbox/pkg/models"
+)
+
+var mockSnippet = &models.Snippet{
+	ID:      1,
+	Title:   "An old silent pond",
+	Content: "An old silent pond...",
+	Created: time.Now(),
+	Expires: time.Now(),
+}
+
+type SnippetModel struct{}
+
+func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
+	switch id {
+	case 1:
+		return mockSnippet, nil
+
+	default:
+		return nil, models.ErrNoRecord
+	}
+}
+func (m *SnippetModel) Lattest() ([]*models.Snippet, error) {
+	return []*models.Snippet{mockSnippet}, nil
+}
---
+++ pkg/models/mock/users.go
@@ -0,0 +1,44 @@
+package mock
+
+import (
+	"time"
+
+	"github.com/Franklynoble/snippetbox/pkg/models"
+)
+
+var mockUser = &models.User{
+	ID:      1,
+	Name:    "Alice",
+	Email:   "alice@gmail.com",
+	Created: time.Now(),
+	Active:  true,
+}
+
+type UserModel struct{}
+
+func (m *UserModel) Insert(name, email, password string) error {
+	switch email {
+	case "dupe@example.com":
+		return models.ErrDuplicateEmail
+	default:
+		return nil
+	}
+}
+
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	switch email {
+	case "alice@gmail.com":
+		return 1, nil
+	default:
+		return 0, models.ErrinvalidCredentials
+	}
+}
+func (m *UserModel) Get(id int) (*models.User, error) {
+	switch id {
+	case 1:
+		return mockUser, nil
+
+	default:
+		return nil, models.ErrNoRecord
+	}
+}
